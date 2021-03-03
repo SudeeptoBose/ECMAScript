f55aff4c9886644499a3d699635f07c5283103d5
SudeeptoBose <sudeeptobose23@gmail.com>
2021-03-03 22:04:19 +0600

    Create class_constructor.js
    Understanding the basics of how a constructor works

---
+++ class_constructor.js
@@ -0,0 +1,12 @@
+// Constructors are called when an object of a class is created
+
+class Intern{
+
+	constructor(){
+		console.log("Hello! I'm the constructor");
+	}
+}
+
+// Create new object
+
+new Intern();
