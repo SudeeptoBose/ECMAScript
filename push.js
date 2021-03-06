00eecca9aa6480bac5d41ab92b1f3a942e99f6c3
SudeeptoBose <sudeeptobose23@gmail.com>
2021-03-06 20:37:20 +0600

    Create push.js

---
+++ push.js
@@ -0,0 +1,9 @@
+let poorCountry = ['bangladesh','srilanka'];
+
+let richCountry = ['america', 'canada', 'spain'];
+
+console.log(richCountry);
+
+richCountry.push(poorCountry);
+
+console.log(richCountry);
