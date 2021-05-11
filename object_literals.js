106f11544c41b667c37ced1ee05279bccc2c58cc
SudeeptoBose <sudeeptobose23@gmail.com>
2021-05-11 22:06:01 +0600

    Create object_literals.js

---
+++ object_literals.js
@@ -0,0 +1,15 @@
+// 
+
+function addressMaker(address){
+    const {city, state} = address;
+
+    const newAddress = {
+        city,
+        state,
+        country:'Bangladesh'
+    };
+
+    console.log(`${newAddress.city}, ${newAddress.state}, ${newAddress.country}`);
+}
+
+addressMaker({city:'Dhaka', state:'Uttara'});
